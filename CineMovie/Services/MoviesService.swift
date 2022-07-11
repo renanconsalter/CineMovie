00851c21e9a97cf5cba1249f527575f806b8a7f2
Renan Consalter <renan.consalter@gmail.com>
2022-07-11 11:58:56 -0300

    feat: add Movies service

---
+++ CineMovie/Services/MoviesService.swift
@@ -0,0 +1,48 @@
+//
+//  MoviesService.swift
+//  CineMovie
+//
+//  Created by Renan Consalter on 28/06/22.
+//
+
+import Foundation
+
+protocol MoviesServiceProtocol {
+    typealias ResultMovies = (Result<MoviesResponse, ErrorHandler>)
+    
+    func getTopRatedMovies(page: Int, completion: @escaping (ResultMovies) -> Void)
+    func getPopularMovies(page: Int, completion: @escaping (ResultMovies) -> Void)
+    func searchMovie(query: String, completion: @escaping (ResultMovies) -> Void)
+    func getMovie(id: Int, completion: @escaping (Result<Movie, ErrorHandler>) -> Void)
+}
+
+final class MoviesService: MoviesServiceProtocol {
+    
+    static let shared = MoviesService()
+    
+    private let httpClient = HttpClient.shared
+    
+    func getTopRatedMovies(page: Int, completion: @escaping (ResultMovies) -> Void) {
+        httpClient.request(endpoint: MoviesEndpoint.getTopRatedMovies(page: page),
+                           model: MoviesResponse.self,
+                           completion: completion)
+    }
+    
+    func getPopularMovies(page: Int, completion: @escaping (ResultMovies) -> Void) {
+        httpClient.request(endpoint: MoviesEndpoint.getPopularMovies(page: page),
+                           model: MoviesResponse.self,
+                           completion: completion)
+    }
+    
+    func searchMovie(query: String, completion: @escaping (ResultMovies) -> Void) {
+        httpClient.request(endpoint: MoviesEndpoint.searchMovie(query: query),
+                           model: MoviesResponse.self,
+                           completion: completion)
+    }
+    
+    func getMovie(id: Int, completion: @escaping (Result<Movie, ErrorHandler>) -> Void) {
+        httpClient.request(endpoint: MoviesEndpoint.getMovieById(id: id),
+                           model: Movie.self,
+                           completion: completion)
+    }
+}
