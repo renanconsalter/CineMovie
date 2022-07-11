ebf2d646102c01d789876874016d7029785f4887
Renan Consalter <renan.consalter@gmail.com>
2022-07-11 11:38:13 -0300

    Initial Commit

---
+++ CineMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineMovie
+//
+//  Created by Renan Consalter on 11/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
