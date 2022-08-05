57a0eb4ea6be002a8316ec243ead16fce04ee02e
Renan Consalter <renan.consalter@gmail.com>
2022-08-05 14:47:58 -0300

    refactor: update view models to support dependency injection and datasource tests

--- CineMovie/ViewModel/ListTopRatedMoviesViewModel.swift
+++ CineMovie/ViewModel/ListTopRatedMoviesViewModel.swift
@@ -20,12 +20,13 @@ final class ListTopRatedMoviesViewModel {
         self.service = service
     }
     
-    private var movies: [Movie] = []
+    var movies: [Movie] = []
+    
+    weak var delegate: ListTopRatedMoviesViewModelDelegate?
+    weak var coordinator: MovieDetailsCoordinatorProtocol?
+    
     private var currentPage: Int = 1
     private var isLoading: Bool = false
-
-    weak var delegate: ListTopRatedMoviesViewModelDelegate?
-    weak var coordinator: ListTopRatedMoviesCoordinator?
     
     func getMovie(at indexPath: IndexPath) -> Movie {
         return movies[indexPath.row]
@@ -42,19 +43,17 @@ final class ListTopRatedMoviesViewModel {
     
     func loadTopRatedMovies() {
         guard !isLoading else { return }
-        
         isLoading = true
         
         service.getTopRatedMovies(page: currentPage) { [weak self] result in
-            guard let self = self else { return }
-            self.isLoading = false
+            self?.isLoading = false
             switch result {
             case .success(let topRatedMovies):
-                self.movies.append(contentsOf: topRatedMovies.results)
-                self.currentPage = topRatedMovies.page + 1
-                self.delegate?.didFindTopRatedMovies()
+                self?.movies.append(contentsOf: topRatedMovies.results)
+                self?.currentPage = topRatedMovies.page + 1
+                self?.delegate?.didFindTopRatedMovies()
             case .failure(let error):
-                self.delegate?.didFail(error: error)
+                self?.delegate?.didFail(error: error)
             }
         }
     }
