a9bfde2ae02c0db715e1d1457f6990f469c468ed
Renan Consalter <renan.consalter@gmail.com>
2022-08-05 14:45:55 -0300

    feat: add spy in all coordinators

---
+++ CineMovieTests/ListTopRatedMovies/ListTopRatedMoviesCoordinatorSpy.swift
@@ -0,0 +1,21 @@
+//
+//  ListTopRatedMoviesCoordinatorSpy.swift
+//  CineMovieTests
+//
+//  Created by Renan Consalter on 03/08/22.
+//
+
+import XCTest
+
+@testable import CineMovie
+
+final class ListTopRatedMoviesCoordinatorSpy: MovieDetailsCoordinatorProtocol {
+
+    private(set) var goToMovieDetailsCalled = false
+    private(set) var goToMovieDetailsPassed: Movie?
+    
+    func goToMovieDetails(with movie: Movie) {
+        goToMovieDetailsCalled = true
+        goToMovieDetailsPassed = movie
+    }
+}
