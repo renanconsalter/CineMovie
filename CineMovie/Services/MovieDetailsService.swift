4a3554ae1ce8474bcb75e769640c5bee63097415
Renan Consalter <renan.consalter@gmail.com>
2022-09-13 19:48:06 -0300

    feat: add individual services to each endpoint

---
+++ CineMovie/Services/MovieDetailsService.swift
@@ -0,0 +1,27 @@
+//
+//  MovieDetailsService.swift
+//  CineMovie
+//
+//  Created by Renan Consalter on 30/08/22.
+//
+
+import Foundation
+
+protocol MovieDetailsServiceProtocol {
+    func getMovie(id: Int, completion: @escaping (Result<Movie, ErrorHandler>) -> Void)
+}
+
+final class MovieDetailsService: MovieDetailsServiceProtocol {
+    
+    private let httpClient: HTTPClientProtocol
+    
+    init(httpClient: HTTPClientProtocol = HTTPClient.shared) {
+        self.httpClient = httpClient
+    }
+
+    func getMovie(id: Int, completion: @escaping (Result<Movie, ErrorHandler>) -> Void) {
+        httpClient.request(endpoint: MovieDetailsEndpoint.getMovie(id: id),
+                           model: Movie.self,
+                           completion: completion)
+    }
+}
