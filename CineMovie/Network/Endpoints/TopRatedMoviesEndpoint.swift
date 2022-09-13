e96f213a766a1174a1799e8fff6ef5538468aa19
Renan Consalter <renan.consalter@gmail.com>
2022-09-13 19:46:23 -0300

    feat: add individual endpoints to conform single responsibility principle

---
+++ CineMovie/Network/Endpoints/TopRatedMoviesEndpoint.swift
@@ -0,0 +1,37 @@
+//
+//  TopRatedMoviesEndpoint.swift
+//  CineMovie
+//
+//  Created by Renan Consalter on 22/08/22.
+//
+
+import Foundation
+
+enum TopRatedMoviesEndpoint {
+    case getTopRatedMovies(page: Int)
+}
+
+extension TopRatedMoviesEndpoint: Endpoint {
+    var path: String {
+        switch self {
+        case .getTopRatedMovies:
+            return "/movie/top_rated"
+        }
+    }
+        
+    var method: HTTPMethod {
+        switch self {
+        case .getTopRatedMovies:
+            return .get
+        }
+    }
+    
+    var queryParams: [URLQueryItem] {
+        switch self {
+        case .getTopRatedMovies(let page):
+            return [
+                URLQueryItem(name: "page", value: "\(page)")
+            ]
+        }
+    }
+}
