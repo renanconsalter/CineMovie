557bcaea1b6922f209f6392bc9677a9c1ce1e996
Renan Consalter <renan.consalter@gmail.com>
2022-09-13 19:43:03 -0300

    feat: add new ones and reorganize constants file

--- CineMovie/Utils/Constants.swift
+++ CineMovie/Utils/Constants.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 struct Constants {
-    
     static let notAvailable = "N/A"
     
     struct Colors {
@@ -20,8 +19,14 @@ struct Constants {
         static let film = "film"
         static let filmFill = "film.fill"
         static let search = "magnifyingglass"
+        static let searchTextFill = "text.magnifyingglass"
         static let close = "xmark"
         static let appLogo = "Logo"
+    }
+    struct Images {
+        static let posterPlaceholder = "PosterPlaceholder"
+        static let backdropPlaceholder = "BackdropPlaceholder"
+        static let noResults = "NoResults"
     }
     struct Menus {
         static let topRated = "Top Rated"
@@ -32,17 +37,17 @@ struct Constants {
         static let emptyStateWelcomeText = "Welcome! Type a movie name to start."
         static let emptyStateNoResultsText = "Sorry! We couldn't find any results."
         static let placeholder = "Search movies"
-        static let noResultsImage = "NoResults"
     }
     struct Alerts {
         static let defaultTitle = "Oops"
-        static let defaultButtonTitle = "OK"
-        static let tryAgainButtonTitle = "Try Again"
+        static let defaultButtonText = "OK"
+        static let tryAgainButtonText = "Try Again"
     }
     struct Identifiers {
         static let searchMoviesTableViewIdentifier = "SearchMoviesTableViewCell"
+        static let popularMoviesCollectionViewFooterIdentifier = "popularMoviesCollectionViewFooterIdentifier"
     }
-    struct ApiImageURL {
+    struct ImageURL {
         static let highQuality = "https://image.tmdb.org/t/p/w780"
         static let mediumQuality = "https://image.tmdb.org/t/p/w342"
         static let lowQuality = "https://image.tmdb.org/t/p/w154"
