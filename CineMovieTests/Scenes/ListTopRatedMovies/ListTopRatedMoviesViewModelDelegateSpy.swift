ca6d9e6503c7f31746700220a3b8f7090bbe759f
Renan Consalter <renan.consalter@gmail.com>
2022-09-16 17:43:08 -0300

    refactor: update delegate spies to match respective viewModel delegate

---
+++ CineMovieTests/Scenes/ListTopRatedMovies/ListTopRatedMoviesViewModelDelegateSpy.swift
@@ -0,0 +1,47 @@
+//
+//  ListTopRatedMoviesViewModelDelegateSpy.swift
+//  CineMovieTests
+//
+//  Created by Renan Consalter on 21/07/22.
+//
+
+import XCTest
+
+@testable import CineMovie
+
+final class ListTopRatedMoviesViewModelDelegateSpy: ListTopRatedMoviesViewModelDelegate {
+    
+    private(set) var showLoadingCalled: Bool = false
+    func showLoading() {
+        showLoadingCalled = true
+    }
+    
+    private(set) var showPaginationLoadingCalled: Bool = false
+    func showPaginationLoading() {
+        showPaginationLoadingCalled = true
+    }
+    
+    private(set) var hideLoadingCalled: Bool = false
+    func hideLoading() {
+        hideLoadingCalled = true
+    }
+    
+    private(set) var reloadDataCalled: Bool = false
+    func reloadData() {
+        reloadDataCalled = true
+    }
+    
+    private(set) var didFailCalled: Bool = false
+    private(set) var didFailCalledErrorPassed: ErrorHandler?
+    func didFail(with error: ErrorHandler) {
+        didFailCalled = true
+        didFailCalledErrorPassed = error
+    }
+    
+    private(set) var setNavigationTitleCalled: Bool = false
+    private(set) var setNavigationTitleValuePassed: String?
+    func setNavigationTitle(to value: String) {
+        setNavigationTitleCalled = true
+        setNavigationTitleValuePassed = value
+    }
+}
--- CineMovieTests/Scenes/MovieDetails/MovieDetailsViewModelDelegateSpy.swift
+++ CineMovieTests/Scenes/MovieDetails/MovieDetailsViewModelDelegateSpy.swift
@@ -10,15 +10,27 @@ import XCTest
 @testable import CineMovie
 
 final class MovieDetailsViewModelDelegateSpy: MovieDetailsViewModelDelegate {
-    
     private(set) var didLoadMovieDetailsCalled: Bool = false
-    private(set) var didFailCalled: Bool = false
     
     func didLoadMovieDetails() {
         didLoadMovieDetailsCalled = true
     }
     
-    func didFail(error: ErrorHandler) {
+    private(set) var didFailCalled: Bool = false
+    
+    func didFail(with error: ErrorHandler) {
         didFailCalled = true
+    }
+    
+    private(set) var showLoadingStateCalled: Bool = false
+    
+    func showLoading() {
+        showLoadingStateCalled = true
+    }
+    
+    private(set) var hideLoadingCalled: Bool = false
+    
+    func hideLoading() {
+        hideLoadingCalled = true
     }
 }
