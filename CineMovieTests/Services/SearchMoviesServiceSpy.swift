134690e02ef1acd64eb72ba49746b29ae0c49f1d
Renan Consalter <renan.consalter@gmail.com>
2022-09-16 17:35:41 -0300

    feat: update serviceSpy to match all scenes services

---
+++ CineMovieTests/Services/SearchMoviesServiceSpy.swift
@@ -0,0 +1,32 @@
+//
+//  SearchMoviesServiceSpy.swift
+//  CineMovieTests
+//
+//  Created by Renan Consalter on 30/08/22.
+//
+
+import XCTest
+
+@testable import CineMovie
+
+final class SearchMoviesServiceSpy: SearchMoviesServiceProtocol {
+    var searchMovieToBeReturned: (Result<MoviesResponse, ErrorHandler>)?
+    private(set) var searchMovieCalled: Bool = false
+    private(set) var searchMovieCallCount: Int = 0
+    private(set) var searchMovieQueryPassedCharacterCount: Int?
+    private(set) var searchMovieQueryPassed: String?
+    
+    func searchMovie(query: String, completion: @escaping (Result<MoviesResponse, ErrorHandler>) -> Void) {
+        
+        searchMovieCalled = true
+        searchMovieCallCount += 1
+        searchMovieQueryPassed = query
+        searchMovieQueryPassedCharacterCount = query.count
+        
+        guard let searchMovieToBeReturned = searchMovieToBeReturned else {
+            return completion(.failure(ErrorHandler.noResponse))
+        }
+        
+        return completion(searchMovieToBeReturned)
+    }
+}
