7b02eb4e33c52ee895c4cfd5d1c9a7995a4b5b57
Renan Consalter <renan.consalter@gmail.com>
2022-09-22 11:55:57 -0300

    fix: adopt swiftlint rules on http client

--- CineMovie/Network/Client/HTTPClient.swift
+++ CineMovie/Network/Client/HTTPClient.swift
@@ -8,46 +8,38 @@
 import Foundation
 
 protocol HTTPClientProtocol {
-    func request<T: Decodable>(endpoint: Endpoint,
-                               model: T.Type,
-                               completion: @escaping (Result<T, ErrorHandler>) -> Void)
+    func request<T: Decodable>(endpoint: Endpoint, model: T.Type, completion: @escaping (Result<T, ErrorHandler>) -> Void)
 }
 
 final class HTTPClient: HTTPClientProtocol {
-    
     static let shared = HTTPClient()
-    
-    func request<T>(endpoint: Endpoint,
-                    model: T.Type,
-                    completion: @escaping (Result<T, ErrorHandler>) -> Void)
-    where T : Decodable {
-        
+
+    func request<T: Decodable>(endpoint: Endpoint, model: T.Type, completion: @escaping (Result<T, ErrorHandler>) -> Void) {
         guard let url = endpoint.completeURL else {
             completion(.failure(.invalidURL))
             return
         }
-        
+
         var request = URLRequest(url: url)
         request.allHTTPHeaderFields = endpoint.header
         request.httpMethod = endpoint.method.rawValue
         request.timeoutInterval = endpoint.timeout
-        
+
         if let body = endpoint.body {
             request.httpBody = try? JSONSerialization.data(withJSONObject: body)
         }
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, _ in
             guard let response = response as? HTTPURLResponse else {
                 completion(.failure(.noResponse))
                 return
             }
-            
+
             guard let data = data else {
                 completion(.failure(.invalidData))
                 return
             }
-            
+
             switch response.statusCode {
             case 200...299:
                 do {
@@ -56,9 +48,6 @@ final class HTTPClient: HTTPClientProtocol {
                 } catch {
                     completion(.failure(.decode))
                 }
-            case 400:
-                completion(.failure(.badRequest))
-                return
             case 401:
                 completion(.failure(.unauthorized))
                 return
@@ -72,7 +61,7 @@ final class HTTPClient: HTTPClientProtocol {
                 completion(.failure(.unexpectedStatusCode))
                 return
             }
-            
-        }.resume()
+        }
+        task.resume()
     }
 }
